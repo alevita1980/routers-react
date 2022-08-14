87590f7be78e245b9054a886176b7426c4538f87
Vergara <h.vergara@accenture.com>
2022-08-14 12:14:55 -0300

    pasaron cosas

---
+++ ejemplos_de_clase/src/components/mpa_components/03_products.js
@@ -0,0 +1,14 @@
+import Browser from "../common/browser";
+
+const ProductsMPA=()=>{
+    return(
+        <div>
+            <h1>
+                <Browser content= "Products!"/>
+            </h1>
+        </div>
+
+    )
+}
+
+export default ProductsMPA;
